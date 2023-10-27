dc2d77ee03e09a3509bda5a09e1d8bab4400c1b4
Baptiste Courtin <baptiste.courtin@inovaxo.fr>
2023-10-27 13:43:20 +0200

    kit + trad english

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,22 +10,54 @@ export const ui = {
   fr: {
     "nav.contact": "À PROPOS",
     "nav.live": "LIENS",
+    "nav.home": "ACCUEIL",
+    "nav.charte": "CHARTE",
 
-    "hireMe.title": "JE VEND MON CUL",
-    "hireMe.accroche":
-      "Je suis à la recherche du prochain poste idéal pour étendre mes horizons artistiques",
+    "kit.charte": "Voir la charte graphique",
+    "kit.press": "Voir le presskit",
+
+    "carrousel.metier": "Étudiant, Designer",
+    "carrousel.voir": "VOIR MES ARTICLES ",
+    "carrousel.articles": "Articles",
+    "carrousel.etudes": "Ans d'études",
+    "carrousel.videos": "Vidéos",
+
+    "lastPost.last": "DERNIER ARTICLE",
 
     "video.title1": "REGADER MA",
     "video.title2": "DERNIÈRE VIDÉO :",
+    "video.view": " vues",
+
+    "tags.all": "Tout",
+
+    "footer.droits": "Tous droits réservés",
+    "footer.retour": "Retour en haut",
   },
+
   en: {
     "nav.contact": "ABOUT",
     "nav.live": "LINKS",
+    "nav.home": "HOMEPAGE",
+    "nav.charte": "CHARTER",
 
-    "hireMe.title": "I SELL MY BUTT",
-    "hireMe.accroche": "Yes please continue",
+    "kit.charte": "See the graphic charter",
+    "kit.press": "See the presskit",
 
-    "video.title": "WATCH MY",
+    "carrousel.metier": "Student, Designer",
+    "carrousel.voir": "SEE MY ARTICLES ",
+    "carrousel.articles": "Articles",
+    "carrousel.etudes": "Years of study",
+    "carrousel.videos": "Videos",
+
+    "lastPost.last": "LAST ARTICLE",
+
+    "video.title1": "WATCH MY",
     "video.title2": "LAST VIDEO :",
+    "video.view": " views",
+
+    "tags.all": "All",
+
+    "footer.droits": "All rights reserved",
+    "footer.retour": "Back to top",
   },
 } as const;
